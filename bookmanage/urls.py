e301175e61026d4837f4ff3886fad169380b4be8
迪丽热bug <lqf1373900785@foxmail.com>
2020-09-07 22:39:21 +0800

    图书管理系统：图书列表、出版社列表、作者列表页面编写

--- bookmanage/urls.py
+++ bookmanage/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     re_path('^$', index),
     path('extend/',extend,name='extend'),
     path('extend2/',extend2,name='extend2'),
+    path('books/',bookList,name='books'),
+    path('publishs/',publishList,name='publishs'),
+    path('authors/',authorList,name='authors'),
 
 ]
--- bookmanage/views.py
+++ bookmanage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from bookmanage import models
 
 def index(request):
     return render(request,'book/index.html')
@@ -10,3 +11,15 @@ def extend(request):
 
 def extend2(request):
     return render(request,'book/extend2.html')
+
+def bookList(request):
+    books = models.Book.objects.all()
+    return render(request,'book/books.html',locals())
+
+def publishList(request):
+    publishs = models.Publish.objects.all()
+    return render(request,'book/publishs.html',locals())
+
+def authorList(request):
+    authors = models.Author.objects.all()
+    return render(request,'book/authors.html',locals())
