5bafcf2dac6adeea57708228ad222bf42c1b188e
迪丽热bug <lqf1373900785@foxmail.com>
2020-09-08 23:07:03 +0800

    书籍的新增功能实现 书籍的编辑功能回显有问题，待解决

--- bookmanage/urls.py
+++ bookmanage/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     re_path('^$', index),
     path('extend/',extend,name='extend'),
     path('extend2/',extend2,name='extend2'),
-    path('books/',bookList,name='books'),
+    path('books/list/',bookList,name='books'),
+    path('books/add.html',addbook,name='addbook'),
+    re_path('books/edit/(\d+)',editbook,name='editbook'),
+    path('books/save/',saveNewbook),
     path('publishs/',publishList,name='publishs'),
     path('authors/',authorList,name='authors'),
 
--- bookmanage/views.py
+++ bookmanage/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse
+
 from bookmanage import models
 
 def index(request):
@@ -15,6 +17,30 @@ def extend2(request):
 def bookList(request):
     books = models.Book.objects.all()
     return render(request,'book/books.html',locals())
+
+def addbook(request):
+    publishs = models.Publish.objects.all()
+    authors = models.Author.objects.all()
+    return render(request,'book/addbook.html',locals())
+
+def saveNewbook(request):
+    name = request.POST.get('name')
+    price = request.POST.get('price')
+    publishTime = request.POST.get('publishTime')
+    publish = request.POST.get('publish')
+    authors = request.POST.getlist('authors')
+    print(name,price,publish,publishTime,authors)
+    book = models.Book.objects.create(name=name,price=price,publishTime=publishTime,publish_id=publish)
+    book.authors.set(authors)
+    book.save()
+    bookListHtml = reverse('book:books')
+    return redirect(bookListHtml)
+
+def editbook(request,bookId):
+    book = models.Book.objects.filter(pk=bookId).first()
+    publishs = models.Publish.objects.all()
+    authors = models.Author.objects.all()
+    return render(request,'book/editbook.html',locals())
 
 def publishList(request):
     publishs = models.Publish.objects.all()
